485ef07ed58523899a4bcda1ce41e8b4b111a845
Srinivash29 <srinivash.s_25am@kgkite.ac.in>
2026-08-19 15:02:23 +0530

    first commit

---
+++ Swap.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a value of A: "))
+b = int(input("Enter a value of B: "))
+print("Before Swapping: A = ",a,"B = ",b)
+t = a
+a = b
+b = t
+print("After Swapping: A = ",a,"B = ",b)
